1ce5c196573240815093ee79ef085ff3f847753e
Dani Perez <daniperez@users.sourceforge.net>
2013-02-01 16:53:19 +0100

    url added to doc.

--- src/test/groovy/org/batcha/gradle/plugins/gitDependencies/SanityTest.groovy
+++ src/test/groovy/org/batcha/gradle/plugins/gitDependencies/SanityTest.groovy
@@ -6,6 +6,12 @@ import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.batcha.gradle.plugins.gitDependencies.GitDependenciesPlugin;
 
+/**
+ * Based on https://github.com/kellyrob99/gradle-jslint-plugin/blob/master/src/test/groovy/org/kar/jslint/gradle/plugin/JSLintPluginTest.groovy
+ * 
+ * @author dperez
+ *
+ */
 class SanityTest {
 
 	private Project project
